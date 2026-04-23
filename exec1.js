98ced3773ec13f9ac2c93078e8f5214fa5e68f36
Expedito525 <expeditolimaferreira59@gmail.com>
2026-04-23 13:31:07 -0300

    java2
    java2

---
+++ exec1.js
@@ -0,0 +1,3 @@
+let exemplo1 = "eu gosto de pao"
+let exemplo2 = 1
+console.log(exemplo1+exemplo1)
---
+++ exemplo1.js
@@ -0,0 +1,4 @@
+let anoatual = 2026
+const dataDeNascimento = 2009
+
+console.log(anoatual-dataDeNascimento)
