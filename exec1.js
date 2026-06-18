8e82ab834774d6fbb77f0910df61a06d77b525eb
Expedito525 <expeditolimaferreira59@gmail.com>
2026-06-18 12:08:04 -0300

    v
    hk

--- exec1.js
+++ exec1.js
@@ -1,3 +1,11 @@
-let exemplo1 = "eu gosto de pao"
-let exemplo2 = 1
-console.log(exemplo1+exemplo1)
+const prompt = require('prompt-sync')();
+const fs = require('fs');
+
+const dados = fs.readFileSync('dados.json', 'utf-8')
+const usuarios = JSON.parse(dados);
+const nome = prompt('digite seu nome: ')
+const idade = parseInt(prompt("qual sua idade:" ))
+
+const novoUsuario = {
+   id: usuarios.lenght
+}
